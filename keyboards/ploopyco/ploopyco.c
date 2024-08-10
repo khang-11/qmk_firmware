0a593f5e95b9193aed8a0ced7bb4496f1e6379e4
Khang Nguyen <khang@Khangs-MacBook-Pro.local>
2024-08-10 16:49:50 +1000

    Revert "DPI to 1700"
    This reverts commit 1e91ac1e5ea684a05ddb6b1d77e240d670fcfde9.

--- keyboards/ploopyco/ploopyco.c
+++ keyboards/ploopyco/ploopyco.c
@@ -36,7 +36,7 @@
 #endif
 
 #ifndef PLOOPY_DPI_OPTIONS
-#    define PLOOPY_DPI_OPTIONS {1700}
+#    define PLOOPY_DPI_OPTIONS {600, 900, 1200, 1600, 2400}
 #    ifndef PLOOPY_DPI_DEFAULT
 #        define PLOOPY_DPI_DEFAULT 1
 #    endif
